66d3c8153ea3e8759052f249713fd67edcfc8163
kathryn-willi <kathryn.willi@colostate.edu>
2023-03-17 11:56:07 -0600

    fix crashing problems

--- data_prep.R
+++ data_prep.R
@@ -3,21 +3,25 @@ library(sf)
 library(nhdplusTools)
 library(mapview)
 library(rmapshaper)
+sf::sf_use_s2(FALSE)
+colors = data.frame(
+  Assessment_Category = c("Not Supporting", "Fully Supporting", "Not Assessed", NA),
+  col = c("#DC851E", "#059FA4", "#A1A522", "#C2CAD7"),
+  Priority = c(1, 2, 3, 4))
 
 # park boundary polygons
-parks <- st_read('data/prep/nps_boundary.shp') %>%
-  select(Park = UNIT_NAME) %>%
-  # st_make_valid() %>%
-  # st_simplify(preserveTopology = TRUE, dTolerance = 0.001) %>%
-  # ms_simplify() %>% 
-  st_transform(4326) %>%
-  saveRDS('data/nps_boundary_lines.RDS')
-parks <- readRDS('data/nps_boundary_lines.RDS')
+# parks <- st_read('data/prep/nps_boundary.shp') %>%
+#   select(Park = UNIT_NAME) %>%
+#   st_transform(4326) %>%
+#   saveRDS('data/nps_boundary_lines.RDS')
+# parks <- readRDS('data/nps_boundary_lines.RDS')
+
 # park boundy polylines
 boundary_lines <- st_read('data/prep/nps_boundary_line.shp') %>%
   select(Park = UNIT_NAME) %>%
   st_transform(4326) %>%
   saveRDS('data/nps_boundary_lines.RDS')
+parks <- readRDS('data/nps_boundary_lines.RDS')
 
 # clipped catchments in attains
 attains_inside <- st_read('data/prep/nps_attains_inside.shp') %>%
@@ -26,9 +30,6 @@ attains_inside <- st_read('data/prep/nps_attains_inside.shp') %>%
          Assessment_Category = ovrllst,
          Impairments = imprmnt,
          Park = UNIT_NAME) %>%
-  # st_make_valid() %>%
-  # st_simplify(preserveTopology = TRUE, dTolerance = 0.001) %>%
-  # ms_simplify() %>%
   st_transform(4326) %>%
   saveRDS('data/nps_attains_inside.RDS')
 attains_inside <- readRDS('data/nps_attains_inside.RDS')
@@ -40,9 +41,6 @@ attains_outside <- st_read('data/prep/nps_attains_outside.shp') %>%
          Assessment_Category = ovrllst,
          Impairments = imprmnt,
          Park = UNIT_NAME) %>%
-  # st_make_valid() %>%
-  # st_simplify(preserveTopology = TRUE, dTolerance = 0.001) %>%
-  # ms_simplify() %>%
   st_transform(4326) %>%
   saveRDS('data/nps_attains_outside.RDS')
 attains_outside <- readRDS('data/nps_attains_outside.RDS')
@@ -53,9 +51,6 @@ unassessed_outside <- st_read('data/prep/nps_raw_outside.shp') %>%
          Park = UNIT_NAME) %>%
   # only keep catchments that aren't listed in attains
   filter(!comid %in% attains_outside$comid & !comid %in% attains_inside$comid) %>%
-  # st_make_valid() %>%
-  # st_simplify(preserveTopology = TRUE, dTolerance = 0.001) %>%
-  # ms_simplify() %>% 
   st_transform(4326) %>%
   saveRDS('data/nps_unassessed_outside.RDS')
 unassessed_outside <- readRDS('data/nps_unassessed_outside.RDS')
@@ -66,18 +61,30 @@ unassessed_inside <- st_read('data/prep/nps_raw_inside.shp') %>%
          Park = UNIT_NAME) %>%
   # only keep catchments that aren't listed in attains
   filter(!comid %in% attains_outside$comid & !comid %in% attains_inside$comid) %>%
-  # st_make_valid() %>%
-  # st_simplify(preserveTopology = TRUE, dTolerance = 0.001) %>%
-  # ms_simplify() %>% 
   st_transform(4326) %>%
   saveRDS('data/nps_unassessed_inside.RDS')
 unassessed_inside <- readRDS('data/nps_unassessed_inside.RDS')
 
 inside <- bind_rows(attains_inside, unassessed_inside) %>%
+  sp::merge(colors, by ="Assessment_Category") %>%
+  arrange(Priority) %>%
+  distinct(comid, .keep_all = TRUE) %>%
   mutate(Assessment_Unit = ifelse(is.na(Assessment_Unit), "None", Assessment_Unit)) %>%
-  mutate(Assessment_Category = ifelse(is.na(Assessment_Category), "No Data", Assessment_Category)) %>%
+  mutate(Assessment_Category = case_when(Assessment_Category == "Fully Supporting" ~ "Good",
+                                         Assessment_Category == "Not Supporting" ~ "Impaired",
+                                         Assessment_Category == "Not Assessed" ~ "Not Assessed",
+                                         is.na(Assessment_Category) ~ "No State Data")) %>% 
   saveRDS('data/nps_inside.RDS')
+inside <- readRDS('data/nps_inside.RDS')
+
 outside <- bind_rows(attains_outside, unassessed_outside) %>%
+  sp::merge(colors, by ="Assessment_Category") %>%
+  arrange(Priority) %>%
+  distinct(Park, comid, .keep_all = TRUE) %>%
   mutate(Assessment_Unit = ifelse(is.na(Assessment_Unit), "None", Assessment_Unit)) %>%
-  mutate(Assessment_Category = ifelse(is.na(Assessment_Category), "No Data", Assessment_Category)) %>%
+  mutate(Assessment_Category = case_when(Assessment_Category == "Fully Supporting" ~ "Good",
+                                         Assessment_Category == "Not Supporting" ~ "Impaired",
+                                         Assessment_Category == "Not Assessed" ~ "Not Assessed",
+                                         is.na(Assessment_Category) ~ "No State Data")) %>% 
   saveRDS('data/nps_outside.RDS')
+outside <- readRDS('data/nps_outside.RDS') 
--- new_app.R
+++ new_app.R
@@ -10,36 +10,42 @@ library(shinyWidgets)
 library(plotly)
 library(shinyBS)
 sf::sf_use_s2(FALSE)
+
 colors = data.frame(
-  Assessment_Category = c("Not Supporting", "Fully Supporting", "Not Assessed", "No Data"),
+  Assessment_Category = c("Impaired", "Good", "Not Assessed", "No State Data"),
   col = c("#DC851E", "#059FA4", "#A1A522", "#C2CAD7"),
-  Factor = c(0, 1, 2, 3)
-)
-boundary_lines <- readRDS('data/nps_boundary_lines.RDS') #%>% filter(Park %in% c("Gauley River National Recreation Area", "Katahdin Woods and Waters National Monument", "Death Valley National Park"))
-inside <- readRDS('data/nps_inside.RDS') %>% sp::merge(colors, by ="Assessment_Category")
-outside <- readRDS('data/nps_outside.RDS') %>% sp::merge(colors, by ="Assessment_Category")
-# attains_outside <- readRDS('data/nps_attains_outside.RDS') %>% sp::merge(colors, by ="Assessment_Category") #%>%  filter(Park %in% c("Gauley River National Recreation Area", "Katahdin Woods and Waters National Monument", "Death Valley National Park"))
-# attains_inside <- readRDS('data/nps_attains_inside.RDS') %>% sp::merge(colors, by ="Assessment_Category") #%>%  filter(Park %in% c("Gauley River National Recreation Area", "Katahdin Woods and Waters National Monument", "Death Valley National Park"))
-# unassessed_outside <- readRDS('data/nps_unassessed_outside.RDS') #%>% filter(Park %in% c("Gauley River National Recreation Area", "Katahdin Woods and Waters National Monument", "Death Valley National Park"))
-# unassessed_inside <- readRDS('data/nps_unassessed_inside.RDS') #%>% filter(Park %in% c("Gauley River National Recreation Area", "Katahdin Woods and Waters National Monument", "Death Valley National Park"))
+  Priority = c(1, 2, 3, 4))
+
+boundary_lines <- readRDS('data/nps_boundary_lines.RDS') 
+inside <- readRDS('data/nps_inside.RDS')
+outside <- readRDS('data/nps_outside.RDS') 
 
 ui <- fluidPage(
   
   titlePanel("National Park Service Water Quality"),
   
-  sidebarLayout(
-    sidebarPanel(#width = 3, 
-                 fluidRow(pickerInput(
-                   inputId = "park", 
-                   label = "Select National Park:",
-                   choices = boundary_lines$Park,
-                   selected = "Gauley River National Recreation Area",
-                   options = list('actions-box' = TRUE),
-                   multiple = FALSE)),
-                 fluidRow(plotOutput("plot2"))
+  fluidRow(
+    column(3, 
+           h6("This is a draft version of a data viewer to accompany 
+                NPCA's evaluation of state water quality assessments and 
+                the National Park Service System. The underlying data used
+                to develop this map comes from the EPA's ATTAINS geospatial
+                database. Currently, the pie chart plots status by catchment 
+                counts, not by catchment area."),
+           fluidRow(pickerInput(
+             inputId = "park", 
+             label = "Select National Park:",
+             choices = sort(boundary_lines$Park),
+             selected = "Gauley River National Recreation Area",
+             options = list('actions-box' = TRUE),
+             multiple = FALSE)),
+           fluidRow(plotOutput("plot2"))
     ),
-    mainPanel(#width = 9,
-              leafletOutput("map2"))
+    column(9,
+           h6(""),
+           h6(""),
+           br(),
+           leafletOutput("map2"), height = "100%")
   )
 )
 
@@ -68,9 +74,9 @@ server <- function(input, output, session) {
     boundary_lines
     
   })
-
+  
   output$plot2 <- renderPlot({
-    
+    validate (need(nrow(insider()) > 0, message="No assessment data currently available at this park unit."))
     pie <- insider() %>%
       st_drop_geometry() %>%
       distinct(Assessment_Unit, .keep_all = TRUE) %>%
@@ -89,7 +95,7 @@ server <- function(input, output, session) {
       scale_fill_manual(values=pie$col) +
       guides(fill=guide_legend(title="Status by Catchment")) +
       theme_void() + # remove background, grid, numeric label
-      theme(text = element_text(size = 20))
+      theme(text = element_text(size = 20)) 
   })
   
   #  generarte the map object 
@@ -99,6 +105,63 @@ server <- function(input, output, session) {
               lat = 40.59085658003177, 
               zoom = 6) %>%
       addProviderTiles("CartoDB.Positron", group = "CartoDB.Positron") %>%
+      addLayersControl(
+        # baseGroups = c("OpenStreetMap", "Light"),
+        overlayGroups = c(
+          "Catchments"
+        ),
+        position = "topleft",
+        options = layersControlOptions(collapsed = TRUE)
+      )
+    # addPolylines(
+    #   data = boundary_liner(),
+    #   fillColor = "",
+    #   fillOpacity = 0,
+    #   color = "black",
+    #   weight = 8) %>%
+    # addPolygons(
+    #   data = insider(),
+    #   fillColor = insider()$col,
+    #   fillOpacity = 1,
+    #   color = "black",
+    #   weight = 1,
+    #   popup = paste(insider()$Assessment_Category,
+    #                 "<br>",
+    #                 insider()$Assessment_Unit,
+    #                 "<br>",
+    #                 ifelse(is.na(insider()$Impairments), "", insider()$Impairments))) %>%
+    # addPolygons(
+    #   data = outsider(),
+    #   fillColor = outsider()$col,
+    #   fillOpacity = 0.15,
+    #   color = "black",
+    #   weight = 1,
+    #   popup = paste0(outsider()$Assessment_Category,
+    #                 "<br>",
+    #                 outsider()$Assessment_Unit,
+    #                 "<br>",
+    #                 ifelse(is.na(outsider()$Impairments), "", outsider()$Impairments)))
+  })
+  
+  # this makes it so the proxy map is rendered in the background, otherwise the map is empty when you first navigate to this page
+  outputOptions(output, "map2", suspendWhenHidden=FALSE)
+  
+  #set location to park
+  observe({
+    c1 <- reactive({
+      boundary_liner() %>%
+        #filter(Park  == input$park) %>%
+        st_set_agr("constant") %>% # attributes constant over geometries (suppresses warning message)
+        st_bbox() %>%
+        unname()
+      #sf::st_centroid() %>%
+      #st_coordinates()
+    })
+    #  
+    leafletProxy('map2') %>% 
+      clearMarkers() %>%
+      clearShapes() %>%
+      fitBounds(lng1 =  c1()[1], lat1 = c1()[2], lng2 =  c1()[3], lat2 = c1()[4]) %>%
       addPolylines(
         data = boundary_liner(),
         fillColor = "",
@@ -107,51 +170,29 @@ server <- function(input, output, session) {
         weight = 8) %>%
       addPolygons(
         data = insider(),
+        group = "Catchments",
         fillColor = insider()$col,
         fillOpacity = 1,
         color = "black",
         weight = 1,
-        popup = paste(insider()$Assessment_Category,
-                      "<br>",
-                      insider()$Assessment_Unit,
-                      "<br>",
-                      ifelse(is.na(insider()$Impairments), "", insider()$Impairments))) %>%
+        popup = paste0("Status: ", insider()$Assessment_Category,
+                       "<br>",
+                       "State ID(s): ", insider()$Assessment_Unit,
+                       "<br>",
+                       "Impairments: ", ifelse(is.na(insider()$Impairments), "None", insider()$Impairments))) %>%
       addPolygons(
         data = outsider(),
         fillColor = outsider()$col,
+        group = "Catchments",
         fillOpacity = 0.15,
         color = "black",
         weight = 1,
-        popup = paste(outsider()$Assessment_Category,
-                      "<br>",
-                      outsider()$Assessment_Unit,
-                      "<br>",
-                      ifelse(is.na(outsider()$Impairments), "", outsider()$Impairments)))
-  })
-  
-  # this makes it so the proxy map is rendered in the background, otherwise the map is empty when you first navigate to this page
-  outputOptions(output, "map2", suspendWhenHidden=FALSE)
-  
-  
-  # add rasters to proxy map
-  observe({
-    leafletProxy("map2") 
-  })
-  
-  #set location to park
-  observeEvent(input$park,{
-    c1 <- reactive({
-     boundary_lines %>%
-        filter(Park  == input$park) %>%
-        st_set_agr("constant") %>% # attributes constant over geometries (suppresses warning message)
-        st_bbox() %>%
-        unname()
-        #sf::st_centroid() %>%
-        #st_coordinates()
-    })
-    # not sure about the zoom level 
-    leafletProxy('map2') %>% 
-      fitBounds(lng1 =  c1()[1], lat1 = c1()[2], lng2 =  c1()[3], lat2 = c1()[4])
+        popup = paste0("Status: ", outsider()$Assessment_Category,
+                       "<br>",
+                       "State ID(s): ", outsider()$Assessment_Unit,
+                       "<br>",
+                       "Impairments: ", ifelse(is.na(outsider()$Impairments), "None", outsider()$Impairments)))
+    
   })
   
   
